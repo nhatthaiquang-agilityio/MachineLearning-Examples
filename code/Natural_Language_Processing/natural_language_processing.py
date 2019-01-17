f1f3f2f25b10b12faddb8e5aaefa287eea7f77e5
Nhat Thai <nhat.thaiquang@asnet.com.vn>
2019-01-17 10:45:56 +0700

    NLP

---
+++ code/Natural_Language_Processing/natural_language_processing.py
@@ -0,0 +1,54 @@
+# Natural Language Processing
+
+# Importing the libraries
+import pandas as pd
+
+# Cleaning the texts
+import re
+import nltk
+
+from nltk.corpus import stopwords
+from nltk.stem.porter import PorterStemmer
+
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.model_selection import train_test_split
+from sklearn.naive_bayes import GaussianNB
+from sklearn.metrics import confusion_matrix
+
+# Importing the dataset
+dataset = pd.read_csv('/code/Natural_Language_Processing/Restaurant_Reviews.tsv', delimiter = '\t', quoting = 3)
+
+
+nltk.download('stopwords')
+
+corpus = []
+for i in range(0, 1000):
+    review = re.sub('[^a-zA-Z]', ' ', dataset['Review'][i])
+    review = review.lower()
+    review = review.split()
+    ps = PorterStemmer()
+    review = [ps.stem(word) for word in review if not word in set(stopwords.words('english'))]
+    review = ' '.join(review)
+    corpus.append(review)
+
+# Creating the Bag of Words model
+cv = CountVectorizer(max_features = 1500)
+X = cv.fit_transform(corpus).toarray()
+y = dataset.iloc[:, 1].values
+
+
+# Splitting the dataset into the Training set and Test set
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.20, random_state = 0)
+
+
+# Fitting Naive Bayes to the Training set
+classifier = GaussianNB()
+classifier.fit(X_train, y_train)
+
+
+# Predicting the Test set results
+y_pred = classifier.predict(X_test)
+
+
+# Making the Confusion Matrix
+cm = confusion_matrix(y_test, y_pred)
