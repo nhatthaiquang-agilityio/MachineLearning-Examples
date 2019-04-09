4c394481c0a19caefbd43df1b9f87259054bdcd2
Nhat Thai <nhat.thaiquang@asnet.com.vn>
2019-04-09 11:14:23 +0700

    update file

--- code/deep_learning/ann.py
+++ code/deep_learning/ann.py
@@ -1,34 +1,19 @@
 # Artificial Neural Network
-
-# Installing Theano
-# pip install --upgrade --no-deps git+git://github.com/Theano/Theano.git
-
-# Installing Tensorflow
-# Install Tensorflow from the website: https://www.tensorflow.org/versions/r0.12/get_started/os_setup.html
-
-# Installing Keras
-# pip install --upgrade keras
-
-# Part 1 - Data Preprocessing
-
-# Importing the libraries
-# import numpy as np
-# import matplotlib.pyplot as plt
 import pandas as pd
+
+from keras.models import Sequential
+from keras.layers import Dense
 
 from sklearn.preprocessing import LabelEncoder, OneHotEncoder, StandardScaler
 from sklearn.metrics import confusion_matrix
 from sklearn.model_selection import train_test_split
 
-# Importing the Keras libraries and packages
-# import keras
-from keras.models import Sequential
-from keras.layers import Dense
 
 # Importing the dataset
 dataset = pd.read_csv('Churn_Modelling.csv')
 X = dataset.iloc[:, 3:13].values
 y = dataset.iloc[:, 13].values
+
 
 # Encoding categorical data
 labelencoder_X_1 = LabelEncoder()
@@ -39,16 +24,18 @@ onehotencoder = OneHotEncoder(categorical_features=[1])
 X = onehotencoder.fit_transform(X).toarray()
 X = X[:, 1:]
 
+
 # Splitting the dataset into the Training set and Test set
 X_train, X_test, y_train, y_test = train_test_split(
     X, y, test_size=0.2, random_state=0)
+
 
 # Feature Scaling
 sc = StandardScaler()
 X_train = sc.fit_transform(X_train)
 X_test = sc.transform(X_test)
 
-# Part 2 - Now let's make the ANN!
+
 # Initialising the ANN
 classifier = Sequential()
 
@@ -69,7 +56,8 @@ classifier.compile(
 # Fitting the ANN to the Training set
 classifier.fit(X_train, y_train, batch_size=10, nb_epoch=100)
 
-# Part 3 - Making the predictions and evaluating the model
+
+# Making the predictions and evaluating the model
 # Predicting the Test set results
 y_pred = classifier.predict(X_test)
 y_pred = (y_pred > 0.5)
